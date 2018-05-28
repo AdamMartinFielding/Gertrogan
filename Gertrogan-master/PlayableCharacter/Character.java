b69721597dd9a68c21b4d609583fede4fc0b4649
ADAM <ADAM@ADAM-PC>
2018-05-28 19:20:08 -0400

    New build with classes implemented.

---
+++ Gertrogan-master/PlayableCharacter/Character.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gertrogan;
+
+/**
+ *
+ * @author Evan
+ */
+public interface Character {
+    
+    public void move(int x, int y);
+    
+    public int getXPos();
+
+    public int getYPos();
+    
+    public int getAttack();
+    
+    public int getHealth();
+
+}
---
+++ Gertrogan/src/gertrogan/BasicEnemy.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gertrogan;
+
+public class BasicEnemy {
+
+    //To do list
+    //
+    
+private int health;
+private int attackPow;
+
+    
+    public BasicEnemy(int xPos, int yPos){
+        health = 100;
+        attackPow = 10;
+        
+        
+        
+    }
+    
+    public int takeDamage(int playerHealth, int enemyDamage) {
+
+        playerHealth = playerHealth - enemyDamage;
+
+        if (playerHealth <= 0) {
+        //Player death method required
+        //death();
+            return 0;
+        } else {
+            return playerHealth;
+        }
+    }
+
+}
