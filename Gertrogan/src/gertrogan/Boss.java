c28e2567938ed111e12f44411cae7ca749abcc2b
Evan <Evan@Gororo-2018>
2018-05-30 14:00:03 -0400

    Basic boss created

---
+++ Gertrogan/src/gertrogan/Boss.java
@@ -0,0 +1,25 @@
+/*
+ Evan Hansen
+ 18/05/30
+ A class to define a boss character in the game
+ */
+package gertrogan;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Evan
+ */
+public class Boss extends BasicEnemy {
+    
+    
+    public Boss(String type, Icon image, int health, int attack, int col, int row){
+        super();
+        
+        
+    }
+    
+    
+}
